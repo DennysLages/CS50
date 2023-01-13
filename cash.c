ed5b0dd688f6d13011614cd561e3a46ef93dfcd6
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-13 22:13:28 +0000

    Fri, Jan 13, 2023, 10:13 PM +00:00

--- cash.c
+++ cash.c
@@ -12,14 +12,14 @@ int main(void)
     coins = take_coins(coins, change, 0.25);
     change = take_change(coins, change, 0.25);
 
-    coins = take_coins(coins, change, 0.25);
-    change = take_change(coins, change, 0.25);
+    coins = take_coins(coins, change, 0.1);
+    change = take_change(coins, change, 0.1);
 
-    coins = take_coins(coins, change, 0.25);
-    change = take_change(coins, change, 0.25);
+    coins = take_coins(coins, change, 0.05);
+    change = take_change(coins, change, 0.05);
 
-    coins = take_coins(coins, change, 0.25);
-    change = take_change(coins, change, 0.25);
+    coins = take_coins(coins, change, 0.01);
+    change = take_change(coins, change, 0.01);
 
     printf("# of coins used: %i\n", coins);
 }
