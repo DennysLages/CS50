73ad6bd2f1afe646d56c14cfa5f9e29d415d276f
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-13 23:58:36 +0000

    Fri, Jan 13, 2023, 11:58 PM +00:00

--- credit.c
+++ credit.c
@@ -9,7 +9,21 @@ int main(void)
     Take number / 10, do this until >> zero
     Atribute mod 10 * 2 to a sum
 
-    for (int i = )
+    int sum = 0;
+    do
+    {
+        int i = number % 10;
+        sum = sum i * 2;
+        number = number / 10;
+    } while(number > 0)
 
+    do
+    {
+        int i = number % 10;
+        sum = sum i * 2;
+        number = number / 10;
+    } while(number > 0)
+
+    printf("%i, sum");
 
 }
