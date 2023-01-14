7296e3ee12c98c99976d8f2937082bda2405bb5c
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-14 14:50:07 +0000

    Sat, Jan 14, 2023, 2:50 PM +00:00

--- aula1/population.c
+++ aula1/population.c
@@ -7,21 +7,21 @@ int main(void)
     int start;
     int end;
 
-    do
+    do  //Get a valid start size
     {
         start = get_int("Start size: \n");
     }
     while (start < 9);
 
-    do
+    do  //Get higher end size
     {
         end = get_int("End size: \n");
     }
     while (start > end);
 
-    for(int i = 0; start < end; i++)
+    for (int i = 0; start < end; i++)   //Calculate years to grow population
     {
-        start += start/3 - start/4;
+        start += start / 3 - start / 4;
         years++;
     }
 
