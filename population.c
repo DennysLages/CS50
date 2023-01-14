e02bd869254b66355170207de4fbef3fbe1d45d0
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-14 13:59:40 +0000

    Sat, Jan 14, 2023, 1:59 PM +00:00

--- population.c
+++ population.c
@@ -4,10 +4,21 @@
 
 int main(void)
 {
-    int start = get_int("Start size: \n");
-    int end = get_int("End size: \n");
+    int years = 0;
 
+    do
+    {
+        int start = get_int("Start size: \n");
+        int end = get_int("End size: \n");
+    }
+    while (start > end);
 
+    do
+    {
+        start += start/3 - start/4;
+        years++;
+    }
+    while (start < end);
 
     prinf("Years: %i\n", years);
 }
