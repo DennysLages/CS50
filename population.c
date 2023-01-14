785da0eff22b382527558ddeb36d4c977ab653b6
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-14 13:54:55 +0000

    Sat, Jan 14, 2023, 1:54 PM +00:00

---
+++ population.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<math.h>
+
+int main(void)
+{
+    int start = get_int("Start size: \n");
+    int end = get_int("End size: \n");
+
+
+
+    prinf("Years: %i\n", years);
+}
