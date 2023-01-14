0849b519973c06b957f62e6f4c27e91c867b4aec
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-14 03:06:40 +0000

    Sat, Jan 14, 2023, 3:06 AM +00:00

--- credit.c
+++ credit.c
@@ -15,7 +15,7 @@ int main(void)
         {
             printf("American Express\n");
         }
-        else if (50 < round (card_number / pow(10,14)) > 56)
+        else if (50 < round (card_number / pow(10,14)) < 56)
         {
             printf("MasterCard\n");
         }
