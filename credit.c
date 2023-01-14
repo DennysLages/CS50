0cf860ddecfdc7275034e0538d31461d99dd3242
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-14 00:09:00 +0000

    Sat, Jan 14, 2023, 12:09 AM +00:00

--- credit.c
+++ credit.c
@@ -9,22 +9,20 @@ int main(void)
     //Take card_number / 10, do this until >> zero
     //Atribute mod 10 * 2 to a sum
 
-     number = card_number; //Assign card_number to a test variable
+    int number = round(card_number / 10); //Assign card_number to a test variable
     int sum = 0;
 
     do
     {
-        int i = number % 10;
-        sum = sum i * 2;
-        number = round(number / 10);
+        sum += (number % 10) * 2;
+        number = round(number / 100);
     } while(number > 0)
 
     number = card_number; //Reassign card_number to a test variable
     do
     {
-        int i = number % 10;
-        sum = sum i * 2;
-        number = round(number / 10);
+        sum += (number % 10);
+        number = round(number / 100);
     } while(number > 0)
 
     printf("%i, sum");
