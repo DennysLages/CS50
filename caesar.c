6119f8714b692a4a36dbb0a96085ccccd9833737
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-18 20:47:18 +0000

    Wed, Jan 18, 2023, 8:47 PM +00:00

--- caesar.c
+++ caesar.c
@@ -4,19 +4,21 @@
 #include <ctype.h> //isaplha
 #include <stdlib.h> //atoi >> converts string to integer
 
+int refuse(string s);
+
 int main(int argc, string argv[]) //argc = # of arguments & argv[] = words received from command line
 {
-    int key = atoi(argv[1]);
-    while (argc != 2 || key <= 0)
+    if(refuse(argv[1]) = 1) //test to refuse problems like more arguments, not an integer, false integer
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
+    int key = atoi(argv[1]); //turns string to an integer
     string plaintext = get_string("plaintext: ");
 
 
-    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    for (int i = 0, n = strlen(plaintext); i < n; i++) //Test if will be higher than z/Z, convert and cipher
     {
         if (isalpha(plaintext[i]))
         {
@@ -36,4 +38,21 @@ int main(int argc, string argv[]) //argc = # of arguments & argv[] = words recei
 
     printf("ciphertext: %s\n", plaintext);
     return 0;
+}
+
+int refuse (string s)
+{
+    if (argc != 2 || key <= 0)
+    {
+        return 1;
+    }
+    for (int j = 0, n = strlen(argv[1]); j < n; j++)
+    {
+        if (argv[1][j] >= '0' || argv[1][j] <= 9)
+        {
+            
+        }
+    }
+
+
 }
