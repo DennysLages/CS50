4119e2b455565c869d0fb95f06808c5f10ee8829
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-18 17:48:12 +0000

    Wed, Jan 18, 2023, 5:48 PM +00:00

--- caesar.c
+++ caesar.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(int argc,string argv[])
+int main(int argc)
 {
-    
+    plaintext = get_string("plaintext: ");
+
+    for(int i = 0, n = strgen(plaintext); i < n, i++)
+    {
+        plaintext[i] += argc;
+    }
+
+    printf("ciphertext: %s", plaintext);
 }
