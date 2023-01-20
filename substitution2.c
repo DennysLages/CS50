823353e3e03bdc5dbcbc523bb612fbc33a7298a6
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-20 22:51:14 +0000

    Fri, Jan 20, 2023, 10:51 PM +00:00

---
+++ substitution2.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //print
+#include <cs50.h> //get_string
+#include <string.h> //strlen
+#include <ctype.h> //isaplha isupper islower
+#include <stdlib.h> //atoi >> converts string to integer
+
+int main(int argc, string argv[])
+{
+    validate_arg
+}
+
+1. Validate Key
+    1.1 Only 2 CLArg
+    1.2 Only Alpha chars
+    1.3 Non Repeatable letters
+2. Get Plaintext
+    2.1 Resume each letter to a position (a or A)
+3. Encipher
+    3.1 Get each letter + key (position)
+4. Print ciphetext
