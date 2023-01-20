2f2d083b43bde7220cff0b63207053eab8b360c0
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-20 21:45:16 +0000

    Fri, Jan 20, 2023, 9:45 PM +00:00

--- substitution.c
+++ substitution.c
@@ -4,11 +4,11 @@
 #include <ctype.h> //isaplha isupper islower
 #include <stdlib.h> //atoi >> converts string to integer
 
-int refuse(int size, string s);
+int validate_key(int size, string s);
 
 int main(int argc, string argv[]) //argc = # of arguments & argv[] = words received from command line
 {
-    int key = refuse(argc, argv[1]);
+    int key = validate_key(argc, argv[1]);
     if (key == -1) //test to refuse problems like more arguments, not an integer, false integer
     {
         printf("Usage: ./caesar key\n");
@@ -44,14 +44,14 @@ int main(int argc, string argv[]) //argc = # of arguments & argv[] = words recei
     return 0;
 }
 
-int refuse(int size, string s) //if argv[1] don`t exist, there will be no refuse()
+int validate_key(int size, string s) //if argv[1] don`t exist, there will be no refuse()
 {
     if (size != 2) //Test wrong number of CLArg
     {
         return -1;
     }
 
-    for (int j = 0, n = strlen(s); j < n; j++) //size = argc, s = argv[1] to test non integer key
+    /*for (int j = 0, n = strlen(s); j < n; j++) //size = argc, s = argv[1] to test non integer key
     {
         if (s[j] < '0' || s[j] > '9')
         {
@@ -59,7 +59,7 @@ int refuse(int size, string s) //if argv[1] don`t exist, there will be no refuse
         }
     }
 
-    int key = atoi(s); //turns string to an integer
+    int key = atoi(s); //turns string to an integer*/
 
-    return key;
+    return 0;
 }
