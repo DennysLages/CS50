6bd7aebd9a0143202bd2806e88f25d64e2bcbe6f
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-21 00:23:01 +0000

    Sat, Jan 21, 2023, 12:23 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -1,6 +1,7 @@
 #include <cs50.h> //get_string
 #include <stdio.h> //printf
 #include <string.h> //strlen
+#include <ctype.h> //islower & isupper
 
 const int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
@@ -30,14 +31,17 @@ int main(void)
 
 int get_points(string player)
 {
-    for (int i = 0, n = strlen(p_1); i++)
+    int position;
+    int sum = 0;
+
+    for (int i = 0, n = strlen(player); i< n; i++)
     {
-        if (islower(player))
+        if (islower(player[i]))
         {
             position = player[i] - 'a';
             sum = points[position];
         }
-        else if (isupper(player))
+        else if (isupper(player[i]))
         {
             position = player[i] - 'A';
             sum = points[position];
@@ -46,8 +50,9 @@ int get_points(string player)
     return sum;
 }
 
+/*
 1. Write table of points to an array;
 2. Get from user a string ("Player 1:\n");
 3. Get from user a string ("Player 2:\n");
 4. Define sum1 & sum2 checking points of each char
-5. Higher wins ("Player 1 or 2 wins!\n");
+5. Higher wins ("Player 1 or 2 wins!\n");*/
