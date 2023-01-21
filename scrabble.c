e784682f8fe9dfa06f0398026267d479030b01ef
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-21 00:01:47 +0000

    Sat, Jan 21, 2023, 12:01 AM +00:00

---
+++ scrabble.c
@@ -0,0 +1,7 @@
+
+
+1. Write table of points to an array;
+2. Get from user a string ("Player 1:\n");
+3. Get from user a string ("Player 2:\n");
+4. Define sum1 & sum2 checking points of each char
+5. Higher wins ("Player %i\n", )
