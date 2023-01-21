d7654406df48d447a9813adf2a49c739bb4fa34c
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-21 00:17:53 +0000

    Sat, Jan 21, 2023, 12:17 AM +00:00

--- scrabble.c
+++ scrabble.c
@@ -1,5 +1,6 @@
 #include <cs50.h> //get_string
 #include <stdio.h> //printf
+#include <string.h> //strlen
 
 const int points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
