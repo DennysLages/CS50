eadeeeadf259390b0cf0b8bf69c4a14dd5d36ebd
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-22 18:28:34 +0000

    Sun, Jan 22, 2023, 6:28 PM +00:00

--- aula3/plurarity/plurality.c
+++ aula3/plurarity/plurality.c
@@ -80,8 +80,9 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    candidate winner;
-    winner = candidates[0];
+    string winner[candidate_count];
+    winner = candidate;
+
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i + 1].votes > winner.votes)
