0b98c97158a1436fa90c0760b0c7b60e7f783ae9
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-22 14:58:29 +0000

    Sun, Jan 22, 2023, 2:58 PM +00:00

---
+++ testplural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+int main(argc, string argv[])
+{
+    candidate candidates[argc];
+
+    int voters = get_int("Number of voters: \n");
+
+    for (i = 0; i < voters; i++)
+    {
+        
+    }
+}
