83cbc63b06390b2606646f12046834cfb7d96065
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-28 16:21:46 +0000

    Sat, Jan 28, 2023, 1:21 PM -03:00

--- testtideman.c
+++ testtideman.c
@@ -7,6 +7,12 @@ for (int i = 0; i < candidate_count; i++)
 {
     for (int j = 0; j < candidate_count; j++)
     {
-        if (ranks[i])
+        for (int k = 0; k < candidate_count; k++)
+        {
+            if (k == ranks[i])
+            {
+                
+            }
+        }
     }
 }
