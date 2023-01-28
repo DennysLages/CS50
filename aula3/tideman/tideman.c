a4b9727a63b6b2515019e1f1de415fb7645adb5e
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-01-28 12:35:32 +0000

    Sat, Jan 28, 2023, 9:35 AM -03:00

--- aula3/tideman/tideman.c
+++ aula3/tideman/tideman.c
@@ -242,7 +242,7 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < pair_count; i++)
     {
         
     }
