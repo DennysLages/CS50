f4162444c3f621faae38676e6638811f80896c0b
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-09-10 22:16:08 +0000

    Sun, Sep 10, 2023, 7:16 PM -03:00

--- cs50/aula4/filter/helpers.c
+++ cs50/aula4/filter/helpers.c
@@ -62,8 +62,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     // RGBTRIPLE column_blur[height];
     // RGBTRIPLE group_blur[2][2];
 
-    RGBTRIPLE avrg_blur;
-    // int avrg_Blue;
+    // RGBTRIPLE avrg_blur;
+    int avrg_Blue;
     int avrg_Green;
     int avrg_Red;
     int h = 1;
