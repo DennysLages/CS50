e4fca0d00150731e407677628a2fcde6a265d66a
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-09-10 17:42:39 +0000

    Sun, Sep 10, 2023, 2:42 PM -03:00

--- cs50/aula4/filter/helpers.c
+++ cs50/aula4/filter/helpers.c
@@ -7,31 +7,24 @@ void copying(DWORD avrg, RGBTRIPLE *rgbt);
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    //int h = 0;
-    /*if(h > height)
+    int h = height;
+    
+    if(h == 0)
     {
         return;
-    }*/
+    }
 
     int avrg = 0;
 
-            // int avrg = round((image[0][0].rgbtBlue + image[0][0].rgbtGreen + image[0][0].rgbtRed)) / 3; // it will colapse 255 byte of 8 bits. need a long byte, if it exists.
     for(int h = 0; h < height; h++)
     {
         for(int w = 0; w < width; w++)
         {
-            avrg = (image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed) / 3; // it will colapse 255 byte of 8 bits. need a long byte, if it exists.
-            // copying(avrg, &image[h][w]);
-            // copy(&avrg, &image[h][w].rgbtGreen);
-            // copy(&avrg, &image[h][w].rgbtRed);
-            // image[h][w].rgbtBlue = avrg;
-            // image[h][w].rgbtGreen = avrg;
-            // image[h][w].rgbtRed = avrg;
-
-            //Need to change data in image.rgbt, not to be lost. Maybe use pointers somehow.
-            //w++;
+            avrg = (image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed) / 3;
+            image[h][w].rgbtBlue = avrg;
+            image[h][w].rgbtGreen = avrg;
+            image[h][w].rgbtRed = avrg;
         }
-        //h++;
     }
 
     //grayscale(h, width, image);
