3e9f39f3fbf7fdf699191e8ec0472e37eccc30df
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-09-10 21:48:36 +0000

    Sun, Sep 10, 2023, 6:48 PM -03:00

--- cs50/aula4/filter/helpers.c
+++ cs50/aula4/filter/helpers.c
@@ -74,75 +74,76 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     }
 
     //blur(h-1, width, image); It can`t be recursive because everytime you call it, it will have a new height, then changing boudaries.
-
-    for(int w = 0; w < width; w++)
+    for(int h = 0; h < height; h++)
     {
-        avrg_Blue = 0; avrg_Green = 0; avrg_Red = 0; //Setting zero to start a new group
-
-        if (h == 0)
+        for(int w = 0; w < width; w++)
         {
-                for(int i = 0; i < 1; i++)
-                {
-                    for(int j = -1; j < 1; j++)
-                    {
-                        avrg_Blue += image[h+i][w+j].rgbtBlue;
-                        avrg_Green += image[h+i][w+j].rgbtGreen;
-                        avrg_Red += image[h+i][w+j].rgbtRed;
-                    }
-                }
+            avrg_Blue = 0; avrg_Green = 0; avrg_Red = 0; //Setting zero to start a new group
 
-        image[h][w].rgbtBlue = round(avrg_Blue / 6);
-        image[h][w].rgbtGreen = round(avrg_Green / 6);
-        image[h][w].rgbtRed = round(avrg_Red / 6);
-        }
-
-        if (h == height)
-        {
-                for(int i = -1; i < 0; i++)
-                {
-                    for(int j = -1; j < 1; j++)
-                    {
-                        avrg_Blue += image[h+i][w+j].rgbtBlue;
-                        avrg_Green += image[h+i][w+j].rgbtGreen;
-                        avrg_Red += image[h+i][w+j].rgbtRed;
-                    }
-                }
-
-        image[h][w].rgbtBlue = round(avrg_Blue / 6);
-        image[h][w].rgbtGreen = round(avrg_Green / 6);
-        image[h][w].rgbtRed = round(avrg_Red / 6);
-        }
-        // if(h == 0 || h == height)
-        // {
-        //     break;
-        // }
-
-        // if(w == 0 || w == width)
-        // {
-        //     break;
-        // }
-
-        for(int i = -1; i < 1; i++)
-        {
-            for(int j = -1; j < 1; j++)
+            if (h == 0)
             {
-                avrg_Blue = image[h+i][w+j].rgbtBlue;
-                avrg_Green = image[h+i][w+j].rgbtGreen;
-                avrg_Red = image[h+i][w+j].rgbtRed;
+                    for(int i = 0; i < 1; i++)
+                    {
+                        for(int j = -1; j < 1; j++)
+                        {
+                            avrg_Blue += image[h+i][w+j].rgbtBlue;
+                            avrg_Green += image[h+i][w+j].rgbtGreen;
+                            avrg_Red += image[h+i][w+j].rgbtRed;
+                        }
+                    }
+
+            image[h][w].rgbtBlue = round(avrg_Blue / 6);
+            image[h][w].rgbtGreen = round(avrg_Green / 6);
+            image[h][w].rgbtRed = round(avrg_Red / 6);
             }
+
+            if (h == height)
+            {
+                    for(int i = -1; i < 0; i++)
+                    {
+                        for(int j = -1; j < 1; j++)
+                        {
+                            avrg_Blue += image[h+i][w+j].rgbtBlue;
+                            avrg_Green += image[h+i][w+j].rgbtGreen;
+                            avrg_Red += image[h+i][w+j].rgbtRed;
+                        }
+                    }
+
+            image[h][w].rgbtBlue = round(avrg_Blue / 6);
+            image[h][w].rgbtGreen = round(avrg_Green / 6);
+            image[h][w].rgbtRed = round(avrg_Red / 6);
+            }
+            // if(h == 0 || h == height)
+            // {
+            //     break;
+            // }
+
+            // if(w == 0 || w == width)
+            // {
+            //     break;
+            // }
+
+            for(int i = -1; i < 1; i++)
+            {
+                for(int j = -1; j < 1; j++)
+                {
+                    avrg_Blue = image[h+i][w+j].rgbtBlue;
+                    avrg_Green = image[h+i][w+j].rgbtGreen;
+                    avrg_Red = image[h+i][w+j].rgbtRed;
+                }
+            }
+
+            image[h][w].rgbtBlue = round(avrg_Blue / 9);
+            image[h][w].rgbtGreen = round(avrg_Green / 9);
+            image[h][w].rgbtRed = round(avrg_Red / 9);
+
+            // avrg_Blue = round(avrg_Blue / 9);
+            // avrg_Green = round(avrg_Green / 9);
+            // avrg_Red = round(avrg_Red / 9);
+
+            // image[h][w] = avrg_blur; // Should be copy in entire row at once ?s
         }
-
-        image[h][w].rgbtBlue = round(avrg_Blue / 9);
-        image[h][w].rgbtGreen = round(avrg_Green / 9);
-        image[h][w].rgbtRed = round(avrg_Red / 9);
-
-        // avrg_Blue = round(avrg_Blue / 9);
-        // avrg_Green = round(avrg_Green / 9);
-        // avrg_Red = round(avrg_Red / 9);
-
-        // image[h][w] = avrg_blur; // Should be copy in entire row at once ?s
     }
-
     return;
 }
 
