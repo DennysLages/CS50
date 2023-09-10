18828e44410a20cb3ef38a3831cb2dfbf1ddae2c
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-09-10 11:38:51 +0000

    Sun, Sep 10, 2023, 8:38 AM -03:00

--- cs50/aula4/filter/helpers.c
+++ cs50/aula4/filter/helpers.c
@@ -5,21 +5,25 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     int h = 0;
     int w = 0;
-    if(h > height)
+    /*if(h > height)
     {
         return;
-    }
+    }*/
 
-    while(w < width, w++)
+    while(h < height)
     {
-        int avrg = (image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed) / 3;
-        image[h][w].rgbtBlue = avrg;
-        image[h][w].rgbtGreen = avrg;
-        image[h][w].rgbtRed = avrg;
+        while(w < width)
+        {
+            int avrg = (image[h][w].rgbtBlue + image[h][w].rgbtGreen + image[h][w].rgbtRed) / 3;
+            *image[h][w].rgbtBlue = avrg;
+            *image[h][w].rgbtGreen = avrg;
+            *image[h][w].rgbtRed = avrg;
+            w++;
+        }
+        h++;
     }
-    h++;
 
-    grayscale(h, width, image);
+    //grayscale(h, width, image);
 
     return;
 }
