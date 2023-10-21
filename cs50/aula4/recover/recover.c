a9bcf1d6d14e2269ae53e95bb1611b8c88f38492
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-10-21 15:01:30 +0000

    Sat, Oct 21, 2023, 12:01 PM -03:00

--- cs50/aula4/recover/recover.c
+++ cs50/aula4/recover/recover.c
@@ -3,9 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-    FILE *file = fopen("card.raw", "r");
-    if (file == NULL)
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) //!file
     {
+        printf("Not possible to open %s.\n", argv[1]);
         return 1;
     }
 
