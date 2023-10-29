1fdfeb4b6c80e2d69d8f48608bec16387229c7e9
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-10-29 16:26:52 +0000

    Sun, Oct 29, 2023, 1:26 PM -03:00

--- cs50/aula4/recover/recover.c
+++ cs50/aula4/recover/recover.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
     }
 
     // Read first three bytes
-    BYTE bytes[3];
-    fread(bytes, sizeof(BYTE), 3, file);
+    BYTE buffer[BLOCK];
+    fread(buffer, sizeof(BYTE), 3, file);
 
     // Check first three bytes
     if ( bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff)
     {
-        
+
     }
     //fread(file, 512, )
     buffer[512];
