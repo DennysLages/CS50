9c598384736137bc06a0947e89a323ad7db370b9
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-10-29 19:52:05 +0000

    Sun, Oct 29, 2023, 4:52 PM -03:00

--- cs50/aula4/recover/recover.c
+++ cs50/aula4/recover/recover.c
@@ -1,6 +1,6 @@
+#include <stdint.h> //Necessary for uint8_t and creation of BYTE
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h> //Necessary for uint8_t and creation of BYTE
 
 typedef uint8_t BYTE;
 const int BLOCK = 512;
@@ -11,12 +11,12 @@ int main(int argc, char *argv[])
     // Check number of arguments and return if not 2
     if (argc != 2)
     {
-        //printf("Use: ./recover card.raw");
+        // printf("Use: ./recover card.raw");
         return 1;
     }
 
     FILE *file = fopen(argv[1], "r");
-    if (file == NULL) //!file
+    if (file == NULL) //! file
     {
         printf("Not possible to open %s.\n", argv[1]);
         return 1;
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
     // [1. Variable Creation]
     // Buffer / Destination / Destination_Name / Track#
-    //Creation of buffer of size 512 bytes
+    // Creation of buffer of size 512 bytes
     BYTE buffer[BLOCK];
 
     // Creation of Destination file
@@ -36,26 +36,25 @@ int main(int argc, char *argv[])
     // Creation of variable to track # of images
     int i = 0;
 
-
     // [2. Iterations / Hard coding]
-    //While not end of file, do
-    //Read all BLOCKS of 512 bytes each
+    // While not end of file, do
+    // Read all BLOCKS of 512 bytes each
     while (fread(&buffer, 1, BLOCK, file))
     {
         // Check first bytes if it is a jpeg and start printing
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            //Function to create a String that will be used as name
+            // Function to create a String that will be used as name
             sprintf(name_img, "%03i.jpg", i);
 
-            //Create a file "img" with name "name_img" and Write that 1 block
+            // Create a file "img" with name "name_img" and Write that 1 block
             img = fopen(name_img, "w");
             fwrite(buffer, 1, BLOCK, img);
             i++;
         }
         else
         {
-            if (i > 0) //If already started writing & is not a new image, just keep appending to current image
+            if (i > 0) // If already started writing & is not a new image, just keep appending to current image
             {
                 fwrite(buffer, 1, BLOCK, img);
             }
