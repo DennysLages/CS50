db2db2f69972a49750875d9c1764eb84a92377c2
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-10-29 16:18:31 +0000

    Sun, Oct 29, 2023, 1:18 PM -03:00

--- cs50/aula4/recover/recover.c
+++ cs50/aula4/recover/recover.c
@@ -7,6 +7,13 @@ const int BLOCK = 512;
 
 int main(int argc, char *argv[])
 {
+    // Check number of arguments and return if not 2
+    if (argc != 2)
+    {
+        //printf("Use: only name of file to recovery");
+        return 2;
+    }
+
     FILE *file = fopen(argv[1], "r");
     if (file == NULL) //!file
     {
