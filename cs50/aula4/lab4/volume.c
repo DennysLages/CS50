ecf6e3be8828506ea3623ef1c9b4fa388043e3e3
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-10-30 23:21:24 +0000

    Mon, Oct 30, 2023, 8:21 PM -03:00

--- cs50/aula4/lab4/volume.c
+++ cs50/aula4/lab4/volume.c
@@ -35,8 +35,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[44];
-    while()
-    fread()
+    while(fread())
+
 
     // TODO: Read samples from input file and write updated data to output file
 
