de37c698927566694f4d6440d72f12289e317765
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-08 20:37:13 +0000

    Wed, Nov 8, 2023, 5:37 PM -03:00

--- cs50/aula6/hello.py
+++ cs50/aula6/hello.py
@@ -1,7 +1,7 @@
-import cs50 from get_string
+from cs50 import get_string
 
-def main()
-name = get_string("What is your name?")
-print(f"hello, {name})
+def main():
+    name = get_string("What is your name?")
+    print(f"hello, {name}")
 
 main()
