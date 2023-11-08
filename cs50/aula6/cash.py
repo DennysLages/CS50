5a97cbba338bb8d77e8fe5efa0b1f42863474055
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-08 21:22:35 +0000

    Wed, Nov 8, 2023, 6:22 PM -03:00

--- cs50/aula6/cash.py
+++ cs50/aula6/cash.py
@@ -6,8 +6,9 @@ def main():
     change = get_float("Change owed: ")
     for coins in [0.25, 0.1, 0.05, 0.01]:
         if change % coins == 0:
-            coin += int change / coins
+            coin += int (change / coins)
             change = change % coins
+    print(coin)
 
 main()
 
