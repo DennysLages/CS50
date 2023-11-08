9ca12e680e8543835c1fc2d7759c8ddc38ad452e
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-08 20:59:16 +0000

    Wed, Nov 8, 2023, 5:59 PM -03:00

--- cs50/aula6/mario/less/mario.py
+++ cs50/aula6/mario/less/mario.py
@@ -1,16 +1,18 @@
 from cs50 import get_int
 
+
 def main():
     while True:
         height = get_int("Height: ")
         if height > 0 and height < 9:
-            break;
+            break
 
-    for i in range(1, height+1):
+    for i in range(1, height + 1):
         for j in range(i, height):
             print(" ", end="")
         for j in range(i, 0, -1):
             print("#", end="")
         print()
+
 
 main()
