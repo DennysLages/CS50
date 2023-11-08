5d0ede002550e873cd147ed57ddc9bcf8e42ec8d
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-08 20:53:50 +0000

    Wed, Nov 8, 2023, 5:53 PM -03:00

--- cs50/aula6/mario/less/mario.py
+++ cs50/aula6/mario/less/mario.py
@@ -2,7 +2,7 @@ from cs50 import get_int
 
 def main():
     height = get_int("Height: ")
-    for i in range(1, height):
+    for i in range(1, height+1):
         for j in range(i, 0, -1):
             print("#", end="")
         print()
