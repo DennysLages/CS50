95012b640285755476d660e5e17c6a76ccaf9039
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-08 21:46:17 +0000

    Wed, Nov 8, 2023, 6:46 PM -03:00

--- cs50/aula6/cash.py
+++ cs50/aula6/cash.py
@@ -3,7 +3,11 @@ from cs50 import get_float
 
 def main():
     coins = 0
-    change = get_float("Change owed: ")
+    while True
+        change = get_float("Change owed: ")
+        if change > 0
+            break
+
     change = int(change*100)
     for coin in [25, 10, 5, 1]:
         if change > 0:
