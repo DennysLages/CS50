5fb76c754267272638da8756f5c120f9e9d81602
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-08 23:39:55 +0000

    Wed, Nov 8, 2023, 8:39 PM -03:00

--- cs50/aula6/credit.py
+++ cs50/aula6/credit.py
@@ -2,12 +2,26 @@ from cs50 import get_int
 from cs50 import get_string
 
 def main():
+    sum = 0
     card = get_string("Number: ")
     if len(card) < 13 or len(card) > 16
         print("INVALID")
         return 0
 
     for i in card
-        number.append(card[i])
+        number.append(int(card[i]))
+
+    for i in range[0, len(card), 2]
+        if number[i] * 2 // 10 == 0
+            sum += number[i] * 2
+        elsif i * 2 // 10 == 1
+            sum += number[i] * 2 + 1
+
+    for i in range[1, len(card), 2]
+        sum += number[i]
+
+    if sum // 10 == 0
         
+
+
 
