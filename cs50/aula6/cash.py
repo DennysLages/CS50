00f9dfa92d98f016514b75b39e019115d2653e83
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-08 21:18:03 +0000

    Wed, Nov 8, 2023, 6:18 PM -03:00

--- cs50/aula6/cash.py
+++ cs50/aula6/cash.py
@@ -2,4 +2,20 @@ from cs50 import get_float
 
 
 def main():
-    get_float
+    int coin = 0
+    change = get_float("Change owed: ")
+    for coins in [0.25, 0.1, 0.05, 0.01]
+        if change mod coins == 0:
+            coin += int change / coins
+            change = change mod coins
+
+main()
+
+
+        # if change mod 0.1 == 0:
+        #     coin = int change / 0.1
+        #     change = change mod 0.1
+        # if change mod 0.05 == 0:
+        #     coin = int change / 0.05
+        #     change = change mod 0.05
+
