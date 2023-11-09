b8eb3197dc56f764f3074435c57fc278d2e6aea6
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-09 01:54:57 +0000

    Wed, Nov 8, 2023, 10:54 PM -03:00

--- cs50/aula6/credit.py
+++ cs50/aula6/credit.py
@@ -13,49 +13,32 @@ def main():
     for i in range(0, len(card)):
         number.append(int(card[i]))
 
-    # print(valid(number,card))
     if (valid(number, card)):
         # print(f"{len(card)} & {card[0]} & {card[1]}")
         # print(f"{number[0]} & {number[1]}")
-        # print("Is Valid")
+
         if len(card) == 15 and number[0] == 3 and (number[1] == 4 or number[1] == 7): # in [4, 7]:
             print("AMEX")
-            # if card[1] in [4, 7]:
-            #     print("AMEX")
-            # else:
-            #     print("INVALID1")
 
         if len(card) == 16 and number[0] == 5 and (number[1] > 0 or number[1] < 6):# in range(1, 5):
             print("MASTERCARD")
-            # if card[1] in range(1, 5):
-            #     print("MASTERCARD")
-            # else:
-            #     print("INVALID2")
 
         if len(card) in [13, 16] and number[0] == 4:
             print("VISA")
         else:
             print("INVALID")
 
-        # if len(card) in [13, 15, 16]:
-        #     print("Test worked")
-
-        # if {card[0]} == 4:
-            # print("Test 2 worked")
-    # print("Reached the end of main function")
-
 def valid(number, card):
 
     sum = 0
-    for i in range(len(card) - 2, 0, -2):
+    for i in range(len(card) - 2, -1, -2):
         sum += (((number[i] * 2) // 10) + (number[i] * 2) % 10)
-        print(f"{number[i]} * 2 = {sum}")
+        # print(f"{number[i]} * 2 = {sum}")
 
-    for i in range(len(card) - 1, 0, -2):
+    for i in range(len(card) - 1, -1, -2):
         # print(number[i])
         sum += number[i]
-        print(f"{number[i]} {sum}")
-        # print(f"{number[i]} * 2 = {sum}")
+        # print(f"{number[i]} {sum}")
 
     if sum % 10 != 0:
         print("INVALID")
