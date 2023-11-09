d67a5af2d7856739a1e2f9856afa63f0634429bb
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-09 01:30:12 +0000

    Wed, Nov 8, 2023, 10:30 PM -03:00

--- cs50/aula6/credit.py
+++ cs50/aula6/credit.py
@@ -17,7 +17,7 @@ def main():
     if (valid(number, card)):
         # print(f"{len(card)} & {card[0]} & {card[1]}")
         # print(f"{number[0]} & {number[1]}")
-        print("Is Valid")
+        # print("Is Valid")
         if len(card) == 15 and number[0] == 3 and number[1] in [4, 7]:
             print("AMEX")
             # if card[1] in [4, 7]:
