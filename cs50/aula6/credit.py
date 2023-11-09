a902c83cc2eea389a081c47656874ccd8f5e3f86
DennysLages <120940056+DennysLages@users.noreply.github.com>
2023-11-09 00:38:58 +0000

    Wed, Nov 8, 2023, 9:38 PM -03:00

--- cs50/aula6/credit.py
+++ cs50/aula6/credit.py
@@ -47,7 +47,7 @@ def valid(number, card):
         print(f"{number[i]} {sum}")
         # print(f"{number[i]} * 2 = {sum}")
 
-    if sum // 10 == 0:
+    if sum % 10 == 0:
         return True
     else:
         print("INVALID")
